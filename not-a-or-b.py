3bcad2c78097ba8f71bf60092bad560ccd1fbb8a
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-06-03 17:18:42 +0530

    De Morgan

---
+++ not-a-or-b.py
@@ -0,0 +1,29 @@
+# not (a is true or b is true) = false
+## true or true is true
+
+# (a is not true and b is not true) = false
+## false and false is false
+
+def notAandB(paramOne, paramTwo):
+    additionOfTwoNumbers = paramOne + paramTwo
+    if not (paramOne <= 10 or paramTwo >= 15):
+        print(f'Addition of two numbers : {additionOfTwoNumbers}')
+    else:
+        print(f'The number is neither less than equal to 10 nor greater than equal to 15')
+     
+
+def notAORnotB(paramOne, paramTwo):
+    additionOfTwoNumbers = paramOne + paramTwo
+    if((paramOne <= 10 and paramTwo >= 15)):
+        print(f'Addition of two numbers : {additionOfTwoNumbers}')
+    else:
+        print(f'The number is neither less than equal to 10 nor greater than equal to 15')
+           
+    
+notAandB(1, 140)
+notAORnotB(11, 14)
+
+'''
+The number is neither less than equal to 10 nor greater than equal to 15
+The number is neither less than equal to 10 nor greater than equal to 15
+'''
