7f4f59e4d997355ba2e7b466ce70bf16bd6176fb
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-06-03 16:02:00 +0530

    First to main

---
+++ numpy-fifth.py
@@ -0,0 +1,21 @@
+import numpy as np
+# coming back to main from first
+list_array = np.array([
+    [2, 5, 9],
+    [45, 12, 23],
+    [78, 985, 36]
+])
+
+print(list_array)
+print(list_array.shape)
+print(list_array.dtype)
+print(list_array.size)
+
+'''
+[[  2   5   9]
+ [ 45  12  23]
+ [ 78 985  36]]
+(3, 3)
+int64
+9
+'''
