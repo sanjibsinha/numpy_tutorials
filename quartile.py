20c0f28ef25570885e0d9ed9a6412d5220395b14
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-06-10 08:35:31 +0530

    Q and Q again

--- quartile.py
+++ quartile.py
@@ -1,9 +1,13 @@
-import numpy
+import numpy as np
 
-values = [13,21,21,40,42,49,55,76, 85]
+values = [70, 80, 85, 90, 95]
 
-quantile_values = numpy.quantile(values, [0,0.25,0.5,0.75,1])
+quantile_values = np.quantile(values, [0,0.25,0.5,0.75,1])
 
 print(f'Quantile values: {quantile_values}')
 
-# Quantile values: [13. 21. 42. 55. 85.]
+# Quantile values: [70. 80. 85. 90. 95.]
+
+print(f'Median Value: {np.median(quantile_values)}')
+
+# Median Value: 85.0
