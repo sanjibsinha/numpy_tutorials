1470441c7aad488100fb45b7f7a52bb2c4485cbc
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-06-15 18:14:09 +0530

    Broadcasting

---
+++ operations/broadcasting.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+distances_in_miles = np.array([4.5, 8.2, 6.4, 0.6])
+distances_in_kilometers = distances_in_miles * 1.6
+
+print(distances_in_kilometers)
+# [ 7.2  13.12 10.24  0.96]
