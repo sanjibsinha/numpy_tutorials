210d1af6ddc774c259285f9de02ce2f5614f721b
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-06-16 17:48:49 +0530

    Matrix prod

---
+++ more-operations/first-op.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+an_array = np.array([1, 2, 3, 4])
+print(an_array.sum())
+print(an_array.prod())
+another_array = np.array([4, 3, 2, 1])
+sum_of_two_arrays = an_array + another_array
+print(sum_of_two_arrays)
+product_of_two_arrays = an_array * another_array
+print(product_of_two_arrays)
+'''
+10
+24
+[5 5 5 5]
+[4 6 6 4]
+'''
